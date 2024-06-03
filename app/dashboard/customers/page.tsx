452ef7a8c90b4caea54c4417024454321e2cf003
SharmaS1402 <kusum9418177125@gmail.com>
2024-06-03 17:58:53 +0530

    dashboard commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+
+export default function page() {
+  return (
+    <div>
+      <p>Customers Page</p>
+    </div>
+  )
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+
+export default function page() {
+  return (
+    <div>
+      <p>Invoices Page</p>
+    </div>
+  )
+}
+
